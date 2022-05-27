9d2586aa026a27024b05e4c082d15897e61fc021
Arthur Béquié <arthurbequie@protonmail.com>
2022-05-27 23:56:09 +0200

    Sort for date label

--- src/lib/components/DataTable/index.tsx
+++ src/lib/components/DataTable/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { RefCallback, useEffect, useState } from "react"
 import DataTableToolbar from "../DataTableToolbar";
 import DataTableHeader from "../DataTableHeader";
 import DataTableBody from "../DataTableBody";
@@ -11,11 +11,54 @@ interface Props {
     rows: Array<Array<string>>
 };
 
+const isDate: Function = (str: string): boolean => {
+    let noSpace = str.replace(/\s/g, '')
+    if( noSpace.length < 3) {
+      return false
+    }
+    return Date.parse(noSpace) > 0
+}
+
+const identifyDataType: Function = (data: string): string => {
+    if(isDate(data)) {
+        return "date"
+    } else if(!isNaN(parseInt(data))) {
+        return "number"
+    } else {
+        return "string"
+    }
+}
+
+const sort: Function = (arrayToSort: Array<Array<string>>, filterByIndex: number): Array<Array<string>> =>  {
+    switch (identifyDataType(arrayToSort[0][filterByIndex])) {
+        case "date":
+            const sortedArray = arrayToSort.sort((a: string[], b: string[]): number => {
+                const date1 = new Date(a[filterByIndex]).getTime()
+                const date2 = new Date(b[filterByIndex]).getTime()
+                
+                return date1 - date2;
+            })
+            return(sortedArray)
+            break
+        case "number":
+            return(arrayToSort)
+            break
+        case "string":
+            return(arrayToSort)
+            break
+        default:
+            return(arrayToSort)
+                
+    }
+}
+
 const DataTable: React.FC<Props> = (props): JSX.Element => {
     const [currentPage, setCurrentPage] = useState<number>(1)
     const [currentItemPerPage, setcurrentItemPerPage] = useState<number>(5)
     const [dataFiltered, setDataFiltered] = useState<Array<Array<string>>>(props.rows)
     const { numberOfPage, dataBatchPerPage } = usePagination(dataFiltered, currentPage, currentItemPerPage)
+
+    console.log(sort(props.rows, 2))
 
     return(
         <>
--- src/lib/components/DataTableHeader/index.tsx
+++ src/lib/components/DataTableHeader/index.tsx
@@ -1,14 +1,26 @@
+import React from "react"
+import ActiveSort from "../../icons/ActiveSort.png"
+import InactiveSort from "../../icons/InactiveSort.png"
+
 interface Props {
     content: Array<string>
 };
 
 const DataTableHeader: React.FC<Props> = (props): JSX.Element => {
-
     return(
         <thead className="data-table-header">
             <tr className="data-table-row">
                 {
-                    props.content.map((item, index) => <th className="data-table-header-cell" scope="col" key={`header-${index}`}>{item}</th>)
+                    props.content.map((item, index) => {
+                        return(
+                            <>
+                                <th className="data-table-header-cell" scope="col" key={`header-${index}`}>
+                                    {item}
+                                    <img src={ActiveSort} alt={"Sort"} style={{width: "15px", height: "15px"}}/>
+                                </th>
+                            </>
+                        )
+                    })
                 }
             </tr>
         </thead>
